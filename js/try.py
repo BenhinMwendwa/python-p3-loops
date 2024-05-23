64b6de1d050c390ca3e367fe7ebf3c434e54eb04
Benhin Mwendwa <benhinmwendwa7@gmail.com>
2024-05-23 18:08:32 +0300

    loops

---
+++ js/try.py
@@ -0,0 +1,6 @@
+player_heights = [0.008, 0.008, 0.008, 0.009, 0.008, 0.01, 0.009, 0.009, 0.01, 0.008, 0.009, 0.009, 0.008, 0.008, 0.008, 0.009, 0.008, 0.009, 0.01, 0.01]
+inch_heights=[]
+for height in player_heights:
+ inch_heights.append(height * 7920)
+
+print(inch_heights)
--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,39 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    countdown = 10
+    while countdown > 0:
+        print(countdown)
+        countdown -= 1
+    print("Happy New Year!")
+
+# Call the function to start the countdown
+happy_new_year()
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    squared_list = [x ** 2 for x in int_list]
+    return squared_list
+
+
+result1 = square_integers([1, 2, 3, 4, 5])
+result2 = square_integers([-1, -2, -3, -4, -5])
+
+print(result1)
+print(result2)  
+
+
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    i = 1
+    while i <=100:
+        if i % 3==0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 5 == 0 :
+            print("Buzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        else:
+            print(i)
+        i+=1
+fizzbuzz()        
+        
